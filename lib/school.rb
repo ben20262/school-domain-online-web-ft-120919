29bd9eabe84acedcd3246a665dc62dbff8583814
Benjamin Small <ben20262@aim.com>
2019-12-16 16:43:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,26 @@
 class School
 
-  roster = {}
+  def roster
+    @roster = {}
+  end
 
   def initialize(school)
     @school = school
   end
 
   def add_student(name, grade)
-    if roster.has_key?(grade)
-      roster[grade] << name
+    if @roster.has_key?(grade)
+      @roster[grade] << name
     else
-      roster[grade] = [name]
+      @roster[grade] = [name]
     end
   end
 
   def grade(grade)
-    roster[grade]
+    @roster[grade]
   end
 
-  # def sort
-  #   roster.each_value{|grade| grade.sort}
-  # end
-
-  def roster
-    roster
+  def sort
+    @roster.each_value{|grade| grade.sort}
   end
 end
